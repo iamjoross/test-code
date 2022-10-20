67a301b887d55232fb4c72589f74fd1c6b655358
Jose Ross Barredo <iamjoross@gmail.com>
2022-10-20 14:26:05 +0800

    Modify test

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,87 +1,95 @@
-
 import pytest
+import requests
 from groups import Groups
 from groups.schema import GroupSchema
 from libraries.redis import Redis
-from nodes import Node, NodesRegistry, handle_ON_CLUSTER_ADD_GROUP, handle_ON_CLUSTER_COMMIT_GROUP, handle_ON_CLUSTER_ROLLBACK_GROUP, setup_event_handlers
+from nodes import (
+    Node,
+    NodesRegistry,
+    handle_ON_CLUSTER_ADD_GROUP,
+    handle_ON_CLUSTER_COMMIT_GROUP,
+    handle_ON_CLUSTER_ROLLBACK_GROUP,
+    setup_event_handlers,
+)
 from event import subscribers
 
+
 def test_create_node_default():
-  id = 'host_1'
-  node = Node(id)
-  assert node.id == id
-  assert isinstance(node.groups, Groups)
+    id = "host_1"
+    node = Node(id)
+    assert node.id == id
+    assert isinstance(node.groups, Groups)
+
 
 def test_create_node_no_param():
-  with pytest.raises(TypeError) as err:
-    node = Node()
+    with pytest.raises(TypeError) as err:
+        node = Node()
+
 
 def test_node_str_representation():
-  id = 'host_1'
-  node = Node(id)
-  assert str(node) == f'<{id}>'
+    id = "host_1"
+    node = Node(id)
+    assert str(node) == f"<{id}>"
+
 
 def test_nodes_registry_build_with_hosts():
-  hosts = ["host1", "host3"]
-  nodes_registry = NodesRegistry.build(hosts)
-  assert len(nodes_registry) == len(hosts)
+    hosts = ["host1", "host3"]
+    nodes_registry = NodesRegistry.build(hosts)
+    assert len(nodes_registry) == len(hosts)
+
 
 def test_nodes_registry_build_without_hosts():
-  nodes_registry = NodesRegistry.build([])
-  assert len(nodes_registry) == len([])
+    nodes_registry = NodesRegistry.build([])
+    assert len(nodes_registry) == len([])
 
-def test_handle_on_cluster_add_group_default():
-  id = 'host_1'
-  node = Node(id)
-  payload= GroupSchema(groupId="group1")
-  error = False
 
-  resp = handle_ON_CLUSTER_ADD_GROUP(node, payload, error)
-  assert resp == True
+@pytest.mark.asyncio
+async def test_handle_on_cluster_add_group_default():
+    id = "host_1"
+    node = Node(id)
+    payload = GroupSchema(groupId="group1")
+    error = False
 
-def test_handle_on_cluster_add_group_with_error():
-  id = 'host_1'
-  node = Node(id)
-  payload= GroupSchema(groupId="group1")
-  error = True
+    await handle_ON_CLUSTER_ADD_GROUP(node, payload, error)
 
-  resp = handle_ON_CLUSTER_ADD_GROUP(node, payload, error)
-  assert resp == False
+
+@pytest.mark.asyncio
+async def test_handle_on_cluster_add_group_with_error():
+    with pytest.raises(requests.exceptions.HTTPError) as err:
+      id = "host_1"
+      node = Node(id)
+      payload = GroupSchema(groupId="group1")
+      error = True
+
+      await handle_ON_CLUSTER_ADD_GROUP(node, payload, error)
 
 
 @pytest.fixture
 def node():
-  node = Node("node1")
-  group = GroupSchema(groupId="group1")
-  node.groups.db._shadow_db.append(group)
-  return node
+    node = Node("node1")
+    group = GroupSchema(groupId="group1")
+    node.groups.db._shadow_db.append(group)
+    return node
+
 
 @pytest.fixture
 def redis():
-  redis = Redis
-  return redis
+    redis = Redis
+    redis['processed_nodes'] = []
+    return redis
 
-def test_handle_on_cluster_rollback_group(node, redis):
-  node1 = node
-  node2 = node
-  redis['processed_nodes'].append(node1)
-  redis['processed_nodes'].append(node2)
 
-  handle_ON_CLUSTER_ROLLBACK_GROUP()
+@pytest.mark.asyncio
+async def test_handle_on_cluster_commit_group(node, redis):
+    node1 = node
+    redis["processed_nodes"].append(node1)
 
-  assert len(node1.groups.db._shadow_db) == 0
-  assert len(node2.groups.db._shadow_db) == 0
-  assert len(redis['processed_nodes']) == 0
+    await handle_ON_CLUSTER_COMMIT_GROUP(node1)
 
-def test_handle_on_cluster_commit_group(node,redis):
-  node1 = node
-  redis['processed_nodes'].append(node1)
+    assert len(node1.groups.db._shadow_db) == 1
+    assert len(redis["processed_nodes"]) == 1
 
-  handle_ON_CLUSTER_COMMIT_GROUP(node1)
-
-  assert len(node1.groups.db._shadow_db) == 1
-  assert len(redis['processed_nodes']) == 0
 
 def test_setup_event_handlers():
-  setup_event_handlers()
-  assert len(subscribers) == 3
+    setup_event_handlers()
+    assert len(subscribers) == 4
